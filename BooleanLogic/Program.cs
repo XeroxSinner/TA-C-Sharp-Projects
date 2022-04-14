cdc9809f026e56916fc428bfbd3e97f84d3f3815
Ny <nyarce805@gmail.com>
2022-04-14 11:53:22 -0500

    Added commenting

--- BooleanLogic/Program.cs
+++ BooleanLogic/Program.cs
@@ -10,13 +10,20 @@ namespace BooleanLogic
     {
         static void Main(string[] args)
         {
+
+            // Input for client's age, converts string to int
             Console.WriteLine("Basic Car Ins. questions using Boolean logic. \nWhat is your age?");
             int age = Convert.ToInt32(Console.ReadLine());
+
+            //Input for DUI - must utilize 'true' or 'false' for string, converts to bool
             Console.WriteLine("Have you ever had a DUI? Please use true or false.");
             bool dui = Convert.ToBoolean(Console.ReadLine().ToLower());
+
+            //Input for tickets, converts to int
             Console.WriteLine("How many speeding tickets do you have?");
             int ticket = Convert.ToInt32(Console.ReadLine());
 
+            //Simple if/else to verify the three requirements
             if (age > 15 && dui == false && ticket < 4)
             {
                 Console.WriteLine("Eligible.");
