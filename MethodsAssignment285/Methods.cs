f790132311a5b37fc16ef69b80c5174f84c68631
Ny <nyarce805@gmail.com>
2022-05-09 14:00:30 -0500

    Methods Assignment 285

---
+++ MethodsAssignment285/Methods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment285
+{
+    public class Methods
+    {
+        //Void method - no output
+        public void Half(double a)
+        {
+            Console.WriteLine("That number halved is: " + (a / 2));
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public string Add(string a, string b)
+        {
+            string s = a + b;
+            return s;
+        }
+    }
+
+    public static class Methods2
+    {
+        public static string fName = "Ny";
+        public static string lName = "Arce";
+        public static void Names()
+        {
+            Console.WriteLine("My name is");
+        }
+    }
+
+    
+}
---
+++ MethodsAssignment285/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment285
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods methods = new Methods();
+            
+
+
+            Console.WriteLine("Input a number, we'll divide it by two: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            methods.Half(a);
+
+            string b = "I'm not sure ";
+            string c = "what's entirely happening.";
+            string s = methods.Add(b, c);
+            Console.WriteLine(s);
+
+            Methods2.Names();
+            Console.WriteLine(Methods2.fName + " " + Methods2.lName);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
