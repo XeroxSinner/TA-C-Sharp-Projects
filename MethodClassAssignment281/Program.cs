587b31fe64fc5e8a11de5ecffd74d166a0e6fcc7
Ny <nyarce805@gmail.com>
2022-05-09 10:28:49 -0500

    Assignment 281

---
+++ MethodClassAssignment281/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment281
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calling method from class
+            VoidMethod voidMethod = new VoidMethod();
+
+            //Calls method, passing in two numbers
+            voidMethod.Params(4, 7);
+
+            //Calls method, specifies parameters by name
+            voidMethod.Params(a: 5, b: 6);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClassAssignment281/VoidMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment281
+{
+    public class VoidMethod
+    {
+        public void Params(int a, int b)
+        {
+            Console.WriteLine("Math on int a: " + (a + 8));
+            Console.WriteLine("Returning int b to screen: " + b);
+            //Console.ReadLine();
+        }
+    }
+}
