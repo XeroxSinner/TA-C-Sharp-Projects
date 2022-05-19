a5e41c11a44ccd0bc5b5a4bbbd77a946f63994b4
Ny <nyarce805@gmail.com>
2022-05-19 12:52:05 -0500

    Abstract Class 302 assignment

---
+++ AbstractClass302/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass302
+{
+    public class Employee : Person
+    {
+        //Utilizes abstract method from Person class and defines what it does
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+
+    }
+}
---
+++ AbstractClass302/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass302
+{
+    public abstract class Person
+    {
+        //Defining properties of Person class
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        //Defining an abstract method that must be used when this class is inherited from
+        public abstract void SayName();
+
+    }
+}
