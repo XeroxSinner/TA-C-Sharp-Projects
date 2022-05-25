0be42329fa6c1aca4303c93f31d3ef96bae4144c
Ny <nyarce805@gmail.com>
2022-05-25 11:48:40 -0500

    Assignment 331

---
+++ StructAssignment331/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment331
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1;
+            num1.Amount = 45.25m;
+
+            Console.WriteLine(num1.Amount);
+            Console.ReadLine();
+
+
+        }
+    }
+
+    struct Number
+    {
+        public decimal Amount;
+    }
+}
