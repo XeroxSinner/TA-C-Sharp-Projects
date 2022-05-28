11b59887077337907601020ddf4eb4b23f72f3d3
Ny <nyarce805@gmail.com>
2022-05-28 15:44:34 -0500

    Assignment 383

---
+++ ConstructorAssign383/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssign383
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var emp = new Employee(13);
+
+            const string myName = "Ny";
+
+            Console.WriteLine("Doesn't do much but tell you my name is {0}.", myName);
+            Console.ReadLine();
+        }
+
+        
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Employee(int id) : this(id, "None")
+        { }
+    
+        public Employee(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
